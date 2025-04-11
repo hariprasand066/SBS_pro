f33bfe95a66abe2173e4dbdfa6956db54d9c9316
hariprasand066 <150042510+hariprasand066@users.noreply.github.com>
2025-04-11 12:16:02 +0530

    Update urls.py

--- SBS_ATTENDANCE/Attendanceportal/attendance/urls.py
+++ SBS_ATTENDANCE/Attendanceportal/attendance/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from . import views
 from attendance.views import (
     AttendanceViewSet, login_view, dashboard_view, logout_view, break_toggle_view
 )
@@ -14,14 +15,14 @@ urlpatterns = [
     path("logout/", logout_view, name="logout"),
     path("break-toggle/", break_toggle_view, name="break_toggle"),
     path("api/", include(router.urls)),
-     path('change-password/', auth_views.PasswordChangeView.as_view(template_name='password.html'), name='password_change'),
+    path('change-password/', auth_views.PasswordChangeView.as_view(template_name='password.html'), name='password_change'),
     path('change-password-done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_done.html'), name='password_change_done'),
-
-    path('api/attendance/start_break/', AttendanceViewSet.as_view({'post': 'start_break'}), name='start_break'),
-    path('api/attendance/end_break/', AttendanceViewSet.as_view({'post': 'end_break'}), name='end_break'),
-    path('api/attendance/logout/', AttendanceViewSet.as_view({'post': 'logout'}), name='logout_api'),
+    path('attendance/start_att_session/', AttendanceViewSet.as_view({'get': 'start_session'}), name='start_attendance_session'),  
+    path('attendance/start_break/', AttendanceViewSet.as_view({'post': 'start_break'}), name='start_break'),
+    path('attendance/end_break/', AttendanceViewSet.as_view({'post': 'end_break'}), name='end_break'),
+    path('attendance/logout/', AttendanceViewSet.as_view({'post': 'logout'}), name='logout_api'),
     path('attendance/get_username/', AttendanceViewSet.as_view({'get': 'get_username'}), name='get_username'),
-    path('api/attendance/auto_logout_count/', AttendanceViewSet.as_view({'get': 'auto_logout_count'}), name='auto_logout_count'),
+    path('attendance/auto_logout_count/', AttendanceViewSet.as_view({'get': 'auto_logout_count'}), name='auto_logout_count'),
 ]
 
 urlpatterns += router.urls  
